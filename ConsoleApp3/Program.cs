9fbfced0276851baabe2e801e5f7fcde4c6fb348
Dragan Pepic <d.pepic@hotmail.com>
2020-06-30 19:59:39 +0200

    asdasd

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -18,23 +18,14 @@ namespace ConsoleApp3
 			string izbor = "";			
 			while(izbor != "5")
 			{
-				Console.WriteLine("1 --- Unos");
-				Console.WriteLine("2 --- Pregled");
-				Console.WriteLine("3 --- Pretraga");
-				Console.WriteLine("4 --- Ukloni");
-				Console.WriteLine("5 --- Izlaz");
-				Console.WriteLine("---------------");
-				Console.Write("Unesite izbor: ");
+				PrikaziMeni();
 				izbor = Console.ReadKey().KeyChar.ToString();
 				Console.WriteLine();
 
 				switch(izbor)
 				{
 					case "1":
-						Console.Write("Unesite ime i prezime: ");
-						Imena.Add(Console.ReadLine());
-						Console.Write("Unesite tel: ");
-						TelBroj.Add(Console.ReadLine());
+						Unos(Imena, TelBroj);
 						break;
 					case "2":
 						for (int indeks = 0; indeks < Imena.Count; indeks++)
@@ -43,28 +34,10 @@ namespace ConsoleApp3
 						}
 						break;
 					case "3":
-						Console.Write("Unesite ime: ");
-						string pretraga = Console.ReadLine();
-						for(int indeks = 0; indeks<Imena.Count; indeks++)
-						{
-							if (Imena[indeks].ToLower().Contains(pretraga.ToLower()))
-							{
-								Console.WriteLine($"{indeks + 1}. {Imena[indeks]} -- {TelBroj[indeks]}");
-							}
-						}
+						Pretraga(Imena, TelBroj);
 						break;
 					case "4":
-						//TODO Treba brisanje po imenu. Moramo da pretrazimo
-						//listu sa imenima, uzmemo indeks (kao i u pretrazi)
-						//pa obrisemo i broj i ime iz lista :) 
-
-						// string.IsNullOrEmpty(NekiString) -- vraca true ako
-						//je string u zagradama prazan
-
-						Console.Write("Unesite broj: ");
-						int ind = int.Parse(Console.ReadLine()) - 1;
-						Imena.RemoveAt(ind);
-						TelBroj.RemoveAt(ind);
+						Brisanje(Imena, TelBroj);
 						break;
 					case "5":
 						Console.WriteLine("Vidimo se :)");
@@ -77,6 +50,53 @@ namespace ConsoleApp3
 
 
 			Console.ReadKey();
+		}
+
+		static void Brisanje (List<string> i, List<string> b)
+		{
+			//TODO Treba brisanje po imenu. Moramo da pretrazimo
+			//listu sa imenima, uzmemo indeks (kao i u pretrazi)
+			//pa obrisemo i broj i ime iz lista :) 
+			
+			// string.IsNullOrEmpty(NekiString) -- vraca true ako
+			//je string u zagradama prazan
+
+			Console.Write("Unesite broj: ");
+			int ind = int.Parse(Console.ReadLine()) - 1;
+			i.RemoveAt(ind);
+			b.RemoveAt(ind);
+		}
+
+		static void Pretraga(List<string> i, List<string> b)
+		{
+			Console.Write("Unesite ime: ");
+			string pretraga = Console.ReadLine();
+			for (int indeks = 0; indeks < i.Count; indeks++)
+			{
+				if (i[indeks].ToLower().Contains(pretraga.ToLower()))
+				{
+					Console.WriteLine($"{indeks + 1}. {i[indeks]} -- {b[indeks]}");
+				}
+			}
+		}
+
+		static void Unos(List<string> Imena, List<string> Brojevi)
+		{
+			Console.Write("Unesite ime i prezime: ");
+			Imena.Add(Console.ReadLine());
+			Console.Write("Unesite tel: ");
+			Brojevi.Add(Console.ReadLine());
+		}
+
+		static void PrikaziMeni()
+		{
+			Console.WriteLine("1 --- Unos");
+			Console.WriteLine("2 --- Pregled");
+			Console.WriteLine("3 --- Pretraga");
+			Console.WriteLine("4 --- Ukloni");
+			Console.WriteLine("5 --- Izlaz");
+			Console.WriteLine("---------------");
+			Console.Write("Unesite izbor: ");
 		}
 	}
 }
